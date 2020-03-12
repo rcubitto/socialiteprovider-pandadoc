04cfd1b2c3189fda28f7ab0aed8cb81c3bf18b5b
Raúl Cubitto <raulcubitto@gmail.com>
2020-03-12 15:16:27 -0300

    wip

--- PandaDocExtendSocialite.php
+++ PandaDocExtendSocialite.php
@@ -6,11 +6,11 @@ use SocialiteProviders\Manager\SocialiteWasCalled;
 
 class PandaDocExtendSocialite
 {
-  /**
-   * Execute the provider.
-   */
-  public function handle(SocialiteWasCalled $socialiteWasCalled)
-  {
-    $socialiteWasCalled->extendSocialite('pandadoc', __NAMESPACE__ . '\Provider');
-  }
+	/**
+	 * Execute the provider.
+	 */
+	public function handle(SocialiteWasCalled $socialiteWasCalled)
+	{
+		$socialiteWasCalled->extendSocialite('pandadoc', __NAMESPACE__ . '\Provider');
+	}
 }
